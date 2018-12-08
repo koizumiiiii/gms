0734a97570d8c9f4fe06f73642218dbec4e2bbc1
EC2 Default User <ec2-user@ip-172-31-26-222.us-east-2.compute.internal>
2018-12-08 17:15:36 +0000

    line module const add

--- app/controllers/line_post.rb
+++ app/controllers/line_post.rb
@@ -1,4 +1,7 @@
 module BatchTest
+  LINE_API = "https://notify-api.line.me/api/notify"
+  LINE_TOKEN = "Bearer HnAsop8EOwkgNMYEa9OIklcon6NGpJ6JqbpFc4fOL2h"
+  
   def post_line
     logger = Logger.new('log/batch_post_line.log')
     logger.info("start post_line batch");
@@ -26,16 +29,16 @@ module BatchTest
 
       # post line
       logger.info("--- post message ---\n#{post_message}");
-      
-      uri = URI.parse("https://notify-api.line.me/api/notify")
+
+      uri = URI.parse(LINE_API)
       https = Net::HTTP.new(uri.host, uri.port)
       https.use_ssl = true
-      
+
       req = Net::HTTP::Post.new(uri.request_uri)
-      req["Authorization"] = "Bearer HnAsop8EOwkgNMYEa9OIklcon6NGpJ6JqbpFc4fOL2h"
+      req["Authorization"] = LINE_TOKEN
       req["Content-Type"] = 'application/x-www-form-urlencoded;charset=UTF-8'
       req.set_form_data({message: post_message})
-      
+
       res = https.request(req)
     end
 
